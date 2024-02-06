97c3acfdd727dac002dd5a7242a039bd8a9bbe5a
Tomoya Suzuki <t3suzuki@ucsd.edu>
2024-02-06 23:22:42 +0000

    up

--- ycsb-cpp.py
+++ ycsb-cpp.py
@@ -8,10 +8,15 @@ WIREDTIGER_PATH = "/home/tomoya-s/mountpoint2/tomoya-s/wiredtiger"
 
 ABT_RESTORE_PATH = "/home/tomoya-s/work/run_rocksdb/staut/abt_backup/abt_restore"
 
-#RECORDCOUNT = 2*1000*1000
-RECORDCOUNT = 100*1000*1000
+RECORDCOUNT = 10*1000*1000
+#RECORDCOUNT = 300*1000
+#RECORDCOUNT = 100*1000*1000
 
 USE_BACKUP = True
+
+def exec_cmd_str(cmd_str):
+    print("Exec: " + cmd_str)
+    subprocess.run(cmd_str.split())
 
 def get_cmd(mode, op, dbengine, n_th, cache_capacity, workload, dbname):
     if dbengine == "rocksdb":
@@ -31,7 +36,7 @@ def get_cmd(mode, op, dbengine, n_th, cache_capacity, workload, dbname):
     return cmd
 
 def run(mode, op, dbengine, n_core, n_th, cache_capacity, workload):
-    subprocess.run("echo mode={}, op={}, dbengine={}, n_core={}, n_th={}, cache_size={}, workload={}".format(mode, op, dbengine, n_core, n_th, cache_capacity, workload).split())
+    exec_cmd_str("echo mode={}, op={}, dbengine={}, n_core={}, n_th={}, cache_size={}, workload={}".format(mode, op, dbengine, n_core, n_th, cache_capacity, workload))
 
     drive_ids = ["0000:0f:00.0","0000:0e:00.0"]
     
@@ -55,8 +60,8 @@ def run(mode, op, dbengine, n_core, n_th, cache_capacity, workload):
     else:
         disable_auto_compactions = 0
         
-    time_sec = 300
-    time_warmup_sec = 30
+    time_sec = 30
+    time_warmup_sec = 10
     make_flags = []
     if dbengine == "rocksdb":
         make_flags.append("BIND_ROCKSDB=1")
@@ -64,14 +69,14 @@ def run(mode, op, dbengine, n_core, n_th, cache_capacity, workload):
         make_flags.append("EXTRA_LDFLAGS=-L{}/build -ldl -lz -lsnappy -lzstd -lbz2 -llz4".format(ROCKSDB_PATH))
     elif dbengine == "wiredtiger":
         make_flags.append("BIND_WIREDTIGER=1")
-        make_flags.append("EXTRA_CXXFLAGS=-I{}/src/include -I{}/build/include -DADD_SCHED_YIELD={} -DTIME_SEC={} -DWT_SESSION_MAX={}".format(WIREDTIGER_PATH, WIREDTIGER_PATH, add_sched_yield, time_sec, n_th))
+        make_flags.append("EXTRA_CXXFLAGS=-I{}/src/include -I{}/build/include -DADD_SCHED_YIELD={} -DTIME_SEC={} -DWT_SESSION_MAX={} -DTIME_WARMUP_SEC={}".format(WIREDTIGER_PATH, WIREDTIGER_PATH, add_sched_yield, time_sec, n_th*2, time_warmup_sec))
         make_flags.append("EXTRA_LDFLAGS=-L{}/build -lwiredtiger".format(WIREDTIGER_PATH))
 
     print(make_flags)
     subprocess.run("make -j -B".split() + make_flags)
         
-    subprocess.run("sudo chcpu -e 1-{}".format(n_core-1).split())
-    subprocess.run("sudo chcpu -d {}-39".format(n_core).split())
+    exec_cmd_str("sudo chcpu -e 1-{}".format(n_core-1))
+    exec_cmd_str("sudo chcpu -d {}-39".format(n_core))
 
 
     if USE_BACKUP:
@@ -79,37 +84,35 @@ def run(mode, op, dbengine, n_core, n_th, cache_capacity, workload):
         if op == "get":
             
             cp_workload_cmd = "cp {workloads_dir}/{workload} {workloads_dir}/cp_workload".format(workloads_dir=workloads_dir, workload=workload)
-            print(cp_workload_cmd)
-            subprocess.run(cp_workload_cmd.split())
+            exec_cmd_str(cp_workload_cmd)
 
             if mode == "abt":
                 restore_env = os.environ.copy()
                 restore_env["DRIVE_IDS"] = "_".join(drive_ids)
                 subprocess.run(ABT_RESTORE_PATH, env=restore_env)
-                subprocess.run("cp {}/myfs_superblock /root/".format(db_dat_path).split())
+                exec_cmd_str("cp {}/myfs_superblock /root/".format(db_dat_path))
             
-            subprocess.run("rm -rf {db_path}".format(db_path=db_path).split())
+            exec_cmd_str("rm -rf {db_path}".format(db_path=db_path))
             cp_db_cmd = "cp -R {db_org_path} {db_path}".format(db_org_path=db_org_path,db_path=db_path)
-            print(cp_db_cmd)
-            subprocess.run(cp_db_cmd.split())
+            exec_cmd_str(cp_db_cmd)
             sec1 = 5
             for x in range(0, sec1):
                 subprocess.run("echo {}/{}".format(x,sec1).split())
                 time.sleep(1)
-            subprocess.run("vmtouch -e {db_path}".format(db_path=db_path).split())
-            subprocess.run("numactl --preferred=0 /home/tomoya-s/work/run_rocksdb/staut/trial/a.out".split())
+            exec_cmd_str("vmtouch -e {db_path}".format(db_path=db_path))
+            exec_cmd_str("numactl --preferred=0 /home/tomoya-s/work/run_rocksdb/staut/trial/a.out")
             sec1 = 10
             for x in range(0, sec1):
                 subprocess.run("echo {}/{}".format(x,sec1).split())
                 time.sleep(1)
     if op == "set":
-        subprocess.run("rm -rf {db_path}".format(db_path=db_path).split())
-        subprocess.run("mkdir -p {db_path}".format(db_path=db_path).split())
+        exec_cmd_str("rm -rf {db_path}".format(db_path=db_path))
+        exec_cmd_str("mkdir -p {db_path}".format(db_path=db_path))
             
     my_env = os.environ.copy()
     if mode == "abt":
         mylib_build_cmd = "make -C {} ABT_PATH={} N_CORE={} ND={} USE_PREEMPT=0".format(MYLIB_PATH, ABT_PATH, n_core, len(drive_ids))
-        process = subprocess.run(mylib_build_cmd.split())
+        process = exec_cmd_str(mylib_build_cmd)
         
         my_env["LD_PRELOAD"] = MYLIB_PATH + "/mylib.so"
         my_env["ABT_PREEMPTION_INTERVAL_USEC"] = "10000000"
@@ -127,17 +130,17 @@ def run(mode, op, dbengine, n_core, n_th, cache_capacity, workload):
         cmd = get_cmd(mode, op, dbengine, n_th, cache_capacity, "cp_workload", db_path)
     else:
         cmd = get_cmd(mode, op, dbengine, n_th, cache_capacity, workload, db_path)
+        
     print(cmd)
-    
     res = subprocess.run(cmd.split(), env=my_env, capture_output=False)
     #print("captured stdout: {}".format(res.stdout.decode()))
     #print("captured stderr: {}".format(res.stderr.decode()))
 
     if op == "set" and dbengine == "rocksdb":
-        subprocess.run("make -f Makefile.compact compact -B N_TH={} DB_PATH={}".format(n_th, db_path).split())
+        exec_cmd_str("make -f Makefile.compact compact -B N_TH={} DB_PATH={}".format(n_th, db_path))
         if mode == "abt":
             mylib_build_cmd = "make -C {} ABT_PATH={} N_CORE={} ND={} USE_PREEMPT=0".format(MYLIB_PATH, ABT_PATH, 1, len(drive_ids))
-            process = subprocess.run(mylib_build_cmd.split())
+            process = exec_cmd_str(mylib_build_cmd)
         
             comp_env = os.environ.copy()
             comp_env["LD_PRELOAD"] = MYLIB_PATH + "/mylib.so"
@@ -147,20 +150,21 @@ def run(mode, op, dbengine, n_core, n_th, cache_capacity, workload):
             comp_env["MYFS_SUPERBLOCK_PATH"] = "/root/myfs_superblock"
             res = subprocess.run("./compact", env=comp_env)
         else:
-            res = subprocess.run("./compact")
+            res = exec_cmd_str("./compact")
 
+    if op == "set":
         if USE_BACKUP:
-            subprocess.run("rm -rf {db_org_path}".format(db_org_path=db_org_path).split())
-            subprocess.run("cp -R {db_path} {db_org_path}".format(db_path=db_path, db_org_path=db_org_path).split())
+            exec_cmd_str("rm -rf {db_org_path}".format(db_org_path=db_org_path))
+            exec_cmd_str("cp -R {db_path} {db_org_path}".format(db_path=db_path, db_org_path=db_org_path))
             if mode == "abt":
-                subprocess.run("cp /root/myfs_superblock {db_org_path}".format(db_org_path=db_org_path).split())
-                subprocess.run("echo Exec abt_backup!".split())
+                exec_cmd_str("cp /root/myfs_superblock {db_org_path}".format(db_org_path=db_org_path))
+                exec_cmd_str("echo Exec abt_backup!")
                 #backup_env = os.environ.copy()
                 #backup_env["DRIVE_IDS"] = "_".join(drive_ids)
                 #subprocess.run(ABT_BACKUP, env=bakup_env)
     
 def run_clean():
-    subprocess.run("dd if=/dev/zero of=/root/myfs_superblock count=1 bs=4G".split())
+    exec_cmd_str("dd if=/dev/zero of=/root/myfs_superblock count=1 bs=4G")
     
 
 workloads = [
@@ -183,31 +187,31 @@ workloads = [
 #     "workloadeu",
 # ]
 
-#cache_size = 10*1024*1024*1024
-cache_size = 1*1024*1024
-mode = "abt"
-#mode = "native"
+cache_size = 10*1024*1024*1024
+#cache_size = 1*1024*1024
+#mode = "abt"
+mode = "native"
 
-#dbengine = "wiredtiger"
-dbengine = "rocksdb"
+dbengine = "wiredtiger"
+#dbengine = "rocksdb"
 
-#run(mode, "set", 1, 1, cache_size, "workloadfu")
 
 #run_clean()
-#run(mode, "set", dbengine, 1, 1, cache_size, "workloadau")
-#subprocess.run("/home/tomoya-s/work/run_rocksdb/staut/trial/a.out")
+#run("native", "set", "wiredtiger", 1, 1, cache_size, "workloadau")
+#run("native", "get", "wiredtiger", 1, 1, cache_size, "workloadau")
 
 #run(mode, "get", dbengine, 8, 128, cache_size, "workloadc")
-#run(mode, "get", dbengine, 8, 128, cache_size, "workloadau")
-#run(mode, "get", dbengine, 8, 128, cache_size, "workloadau")
+#run("native", "get", dbengine, 8, 128, cache_size, "workloada")
+#run("abt", "get", dbengine, 8, 128, cache_size, "workloada")
+
 
 #run_clean()
 #run("abt", "set", dbengine, 1, 1, 1*1024*1024, "workloadau")
 #run("abt", "get", dbengine, 8, 256, 1*1024*1024, "workloadau")
 
 if True:
-    for n_ctx in [128, 256, 64, 32]:
-        for cache_size in [1*1024*1024, 10*1024*1024*1024]:
+    for n_ctx in [128]:
+        for cache_size in [10*1024*1024*1024]: #[1*1024*1024, 10*1024*1024*1024]:
             for workload in workloads:
                 for i in [0]:
                     run(mode, "get", dbengine, 8, n_ctx, cache_size, workload)
